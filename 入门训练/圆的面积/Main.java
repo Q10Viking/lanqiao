48c9011f752aa9162ef11660cea6063d9c76de5b
Q10Viking <1403090523@cau.edu.cn>
2017-12-09 22:19:19 +0800

    commit

---
+++ 入门训练/圆的面积/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	final static double PI=3.14159265358979323;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner stdin = new Scanner(System.in);
+		double R = stdin.nextDouble();
+		double AreaCircle = PI*R*R;
+		System.out.println(String.format("%.7f", AreaCircle));
+	}
+
+}
+
+/*
+4
+50.2654825
+*/
