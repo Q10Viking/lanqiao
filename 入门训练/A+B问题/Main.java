92c21ba6d441c0cfd8a3d09f5300eb38880e1b24
Q10Viking <1403090523@cau.edu.cn>
2017-12-09 22:03:00 +0800

    first commit

---
+++ 入门训练/A+B问题/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+	
+	public static void main(String[] args){
+				Scanner stdin = new Scanner(System.in);
+				int numA = stdin.nextInt();
+				int numB = stdin.nextInt();
+				System.out.println(numA+numB);
+	
+	
+	}
+}
